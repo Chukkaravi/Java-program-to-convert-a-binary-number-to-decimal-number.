d930f72fc2393549e13d63311a152db0db78f562
Chukka Venkata Ravi <102050741+Chukkaravi@users.noreply.github.com>
2023-01-15 09:40:27 +0530

    Add files via upload

---
+++ Assessmentquestion10.java
@@ -0,0 +1,9 @@
+//Write a Java program to convert a binary number to decimal number.
+
+public class Assessmentquestion10{  
+    public static void main(String args[]){  
+        String binaryString="1010";  
+        int decimal=Integer.parseInt(binaryString,2);  
+        System.out.println(decimal);  
+    }
+}  
